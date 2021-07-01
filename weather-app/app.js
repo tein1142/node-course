e03f3fb15179c1759390f6ff11f8225780e17b84
tein1142 <tein1142@hotmail.com>
2021-07-01 19:20:20 +0700

    [Update]34-Handling Errors

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,32 @@
 const request = require('request')
 const chalk = require('chalk')
 
-const urlAPI = 'http://api.weatherstack.com/current?access_key=97d8e8759f7adc753c01e763dffb3afd&query=13.7406803,100.4862916&units=m'
+// const weatherAPI = 'http://api.weatherstack.com/current?access_key=97d8e8759f7adc753c01e763dffb3afd&query=13.7406803,100.4862916&units=m'
 
-request({url: urlAPI, json: true},(error,respose)=> {
-    //    console.log(respose.body)     
-    console.log(`${respose.body.current.weather_descriptions[0]}. It is currently ${chalk.bgBlue.inverse(respose.body.current.temperature)} celsius. There is a ${chalk.bgBlue.inverse(respose.body.current.precip+'%')} chance of rain`)
+// request({url: weatherAPI, json: true},(error,respose)=> {
+//     if(error){
+//         console.log('Unable to connect to weather service')
+//     } else if(respose.body.error){
+//         console.log('Unable to find location')
+//     } 
+//     else{
+//     console.log(`${respose.body.current.weather_descriptions[0]}. It is currently ${chalk.bgBlue.inverse(respose.body.current.temperature)} celsius. There is a ${chalk.bgBlue.inverse(respose.body.current.precip+'%')} chance of rain`)
+//     }
+// })
+
+// Geocoding
+const mapBoxAPI = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoidGVpbjExNDIiLCJhIjoiY2txa3Qyczh6MDJ6MzJ1cGx2NzRlNmR1YSJ9.yIDLTZ2-013BGUtLlPTlLw'
+request({url: mapBoxAPI, json: true},(error,response) => {
+    if(error){
+        console.log('Unable to connect Mapbox service')
+    }else if(response.body.features.length = []){
+        console.log('Unable to find this place')
+    }
+    else{
+        const longitude = chalk.bgCyan(response.body.features[0].bbox[0])
+        const latitude = chalk.bgCyan(response.body.features[0].bbox[1])
+            console.log(longitude)
+            console.log(latitude)
+    }
+
 })
