dcd82de0fa3041351c7f17332180ba7368ba3638
tein1142 <tein1142@hotmail.com>
2021-07-03 14:50:55 +0700

    [ADD]callback

---
+++ playground/4-callbacks.js
@@ -0,0 +1,37 @@
+setTimeout(() => {
+    console.log('Three seconds are up')
+    // console.log(shortNames)
+}, 3000)
+
+const names = ['Tien', 'Nam', 'JJ']
+const shortNames = names.filter((name) => {
+    return name.length <= 4
+})
+
+const geocode = (address, callback) => {
+    setTimeout(() => {
+        const data = {
+            latitude: 0,
+            longitude: 0,
+            address: address
+        }
+        callback(data)
+    }, 2000)
+
+}
+
+geocode('Bangkok', (data) => {
+console.log(data)
+})
+
+
+
+const add = (number1,number2,callback) => {
+    setTimeout(() => {
+        callback(number1 + number2)
+    }, 2000)
+    
+}
+add(1, 4, (sum) => {
+    console.log(sum)
+})
