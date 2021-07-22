5ecac301769fd15fe8fa46b1301c8bc8c8436958
PaNTaVIC HANGNARAN <tein1142@hotmail.com>
2021-07-22 16:04:58 +0700

    [ADD] Dynamic Pages with Templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,17 +1,36 @@
 const express = require('express')
 const path = require('path')
-const app = express()
 
+
+const app = express()
 console.log(__dirname)
 console.log(path.join(__dirname, '../public'))
 
 const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
 
+
 app.get('', (req, res) => {
-    res.send('Hello express!')
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Pantavit'
+
+    })
+})
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Pantavit Hangnalen'
+    })
 })
 
+app.get('/help', (req, res) => {
+    res.render('help' ,{
+        helpText: 'This page can help your problem'
+    })
+})
 app.get('/weather', (req, res) => {
     res.send({
         forecast: 'It is hot',
