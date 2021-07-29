cc3b0ff0b5a3080b7dc7e0c211c600f5380c48c1
PaNTaVIC HANGNARAN <tein1142@hotmail.com>
2021-07-29 20:43:10 +0700

    [ADD] mongoose

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+const validator = require('validator')
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -7,22 +7,80 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 })
 
 // how to create set table and save that value.
-// const User = mongoose.model('User', {
-//     name: {
-//         type: String
-//     },
-//     age: {
-//         type: Number
-//     }
-// })
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        require: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        validate(value) {
+            if (value.toLowerCase() === 'password') {
+                throw Error('Try again to strong password')
+            }
+        },
+        trim: true
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value <= 0) {
+                throw new Error('Age must be positive number')
+            }
+        }
+    }
+})
 
 // const me = new User({
-//     name: 'Nam',
-//     age: 21
+//     name: '  sine',
+//     age: 28,
+//     email: '  Sine.925@hotmail.com',
+//     password: 'sine.925'
 // })
 
 // me.save().then(() => {
 //     console.log(me)
-// }).catch((err) => {
-//     console.log('Error' + err)
+// }).catch((error) => {
+//     console.log('Error' + error)
 // })
+
+
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String,
+        required: true,
+        trim: true,
+
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
+})
+
+const task = new Task({
+    description: '  learn node js',
+
+})
+
+task.save().then(() => {
+    console.log(task)
+}).catch((err) => {
+    console.log('Error' + err)
+})
