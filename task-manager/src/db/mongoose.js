e78d8cdc22ba6c7780eb22f2593735e3d42785fd
PaNTaVIC HANGNARAN <tein1142@hotmail.com>
2021-07-30 17:24:57 +0700

    [Update] Rest API (Post,Get)

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -20,16 +20,3 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 // }).catch((error) => {
 //     console.log('Error' + error)
 // })
-
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        required: true,
-        trim: true,
-
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-})
--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
+const Task = require('./models/task')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -10,11 +11,63 @@ app.use(express.json())
 app.post('/users', (req, res) => {
     const user = new User(req.body)
     user.save().then(() => {
-        res.send(user)
+        res.status(201).send(user)
     }).catch((e) => {
         res.status(400).send(e)
     })
 })
+
+app.get(`/getAllUsers`, (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((e) => {
+        res.status(500).send(e)
+    })
+})
+
+app.get('/getUserById/:id', (req, res) => {
+    const id = req.params.id
+    console.log(id)
+    User.findById(id).then((user) => {
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body)
+    task.save().then(() => {
+        res.status(201).send(task)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+})
+
+app.get(`/AllTasks`, (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((e) => {
+        res.status(500).send(e)
+    })
+})
+
+app.get('/getTaskById/:id', (req, res) => {
+    const id = req.params.id
+    Task.findById(id).then((task) => {
+        res.send(task)
+    }).catch((e) => {
+        res.send(e)
+    })
+})
+
+
+
+
+
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
